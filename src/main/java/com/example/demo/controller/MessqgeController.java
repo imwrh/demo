e92ab64fe5f63e9271f0e391de5ed54d377164f2
1938647832@qq.com <1938647832@qq.com>
2023-06-27 13:30:39 +0800

    更改

--- src/main/java/com/example/demo/controller/MessqgeController.java
+++ src/main/java/com/example/demo/controller/MessqgeController.java
@@ -4,10 +4,7 @@ import com.example.demo.entity.Message;
 import com.example.demo.service.MessageService;
 import com.example.demo.util.PageData;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
@@ -19,10 +16,17 @@ public class MessqgeController {
     @Resource
     private MessageService messageService;
 
-    public ResponseEntity<PageData<Message>> getMessagePage(@PathVariable("count") int count, @PathVariable("pageSize") int pageSize,
-                                         @PathVariable("beforeTime") LocalDateTime beforeTime, @PathVariable("afterTime") LocalDateTime afterTime){
+   @GetMapping("/getMessagePage")
+   public ResponseEntity<PageData<Message>> getMessagePage(@RequestParam("count") int count,
+                                           @RequestParam("pageSize") int pageSize,@RequestParam("beforeTime")  LocalDateTime beforeTime,
+                                           @RequestParam("afterTime")  LocalDateTime afterTime){
+       return ResponseEntity.ok( messageService.getMessagePage(count,pageSize,beforeTime,afterTime));
+   }
 
-        return ResponseEntity.ok(messageService.getMessagePage(count,pageSize,beforeTime,afterTime));
-    }
+   @GetMapping("/id")
+    public ResponseEntity updateMessageType(@PathVariable("id") int id){
+       messageService.updateMessageType(id);
+       return ResponseEntity.ok().build();
+   }
 
 }
