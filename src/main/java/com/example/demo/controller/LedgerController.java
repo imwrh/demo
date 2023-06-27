afaa23d95d2aaf40bf531dd308a7e46236114a3c
1938647832@qq.com <1938647832@qq.com>
2023-06-27 13:20:59 +0800

    更改

--- src/main/java/com/example/demo/controller/LedgerController.java
+++ src/main/java/com/example/demo/controller/LedgerController.java
@@ -21,7 +21,8 @@ public class LedgerController {
     private Ledger ledger;
 
     @GetMapping("/performStockOut")
-    public ResponseEntity<Integer>  performStockOut(@PathVariable("productId") int productId,@PathVariable("num") int num,@PathVariable("storeId") int storeId){
+    public ResponseEntity<Integer>  performStockOut(@RequestParam("productId") int productId,
+                                                    @RequestParam("num") int num,@RequestParam("storeId") int storeId){
 
 
 
@@ -29,20 +30,22 @@ public class LedgerController {
     }
 
     @GetMapping("/performStockIn")
-    public ResponseEntity<Integer> performStockIn(int productId, int num, int storeId){
+    public ResponseEntity<Integer> performStockIn(@RequestParam("productId") int productId,
+                                                  @RequestParam("num") int num,
+                                                  @RequestParam("storeId") int storeId) {
 
         return ResponseEntity.ok(ledgerService.performStockIn(productId,num,storeId));
     }
 
-    public ResponseEntity<PageData<Ledger>> getLedgerByCriteria(@PathVariable("productName") String productName,
-                                                                @PathVariable("startDate") Date startDate,
-                                                                @PathVariable("offset") int offset,
-                                                                @PathVariable("pageSize") int pageSize){
+    public ResponseEntity<PageData<Ledger>> getLedgerByCriteria(@RequestParam("productName") String productName,
+                                                                @RequestParam("startDate") Date startDate,
+                                                                @RequestParam("offset") int offset,
+                                                                @RequestParam("pageSize") int pageSize){
         return ResponseEntity.ok(ledgerService.getLedgerByCriteria(productName,startDate,offset,pageSize));
     }
 
     @GetMapping("/Totalprice")
-    public ResponseEntity<Map> Totalprice(@PathVariable("productId") int productId){
+    public ResponseEntity<Map<Object,Object>> Totalprice(@RequestParam("productId") int productId){
         return ResponseEntity.ok(ledgerService.Totalprice(productId));
     }
 }
